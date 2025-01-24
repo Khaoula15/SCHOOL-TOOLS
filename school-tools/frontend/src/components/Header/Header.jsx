4eb9566f22f46aa1614471c8d8843df061f21e6e
Khaoula15 <khaoulasmp@gmail.com>
2025-01-24 18:46:20 +0100

    second commit

---
+++ school-tools/frontend/src/components/Header/Header.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div className='header'>
+            <div className='header-contents'>
+                <h2>Your Shop for School Essentials!</h2>
+                <p>Get everything you need for the school year in one place! From notebooks and backpacks to art supplies and tech gear, we’ve got the best tools to help you succeed. Shop now and be ready for every class, project, and adventure!</p>
+                <button>View Items</button>
+            </div>
+        </div>
+    )
+}
+
+export default Header
---
+++ school-tools/frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,21 @@
+import React, { useState } from 'react'
+import Header from '../../components/Header/Header'
+import ExploreMenu from '../../components/ExploreMenu/ExploreMenu'
+import ProductDisplay from '../../components/ProductDisplay/ProductDisplay'
+import AppDownload from '../../components/AppDownload/AppDownload'
+
+const Home = () => {
+
+  const [category,setCategory] = useState("All")
+
+  return (
+    <>
+      <Header/>
+      <ExploreMenu setCategory={setCategory} category={category}/>
+      <ProductDisplay category={category}/>
+      <AppDownload/>
+    </>
+  )
+}
+
+export default Home
